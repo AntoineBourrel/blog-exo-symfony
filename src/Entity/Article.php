3b416c3b63fe791a237290af23fdb16a095c51d6
Antoine BOURREL <94041592+AntoineBourrel@users.noreply.github.com>
2022-07-06 10:35:21 +0200

    comment database

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -9,17 +9,20 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Article
 {
-
+    // Paramètre de la colonne Id de la table Article
     /**
      * @ORM\Column(type="integer")
      * @ORM\Id()
      * @ORM\GeneratedValue()
      */
+    // Création de la colonne Id
     public $id;
 
+    // Paramètre de la colonne Title de la table Article
     /**
      * @ORM\Column(type="string")
      */
+    // Création de la colonne Title
     public $title;
 
 }
